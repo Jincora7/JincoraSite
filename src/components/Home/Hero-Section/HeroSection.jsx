fd3bb6925b16c15d8260abd987b24a2bd4788e9b
Adarsh Dhanware <adarshdhanware@gmail.com>
2025-05-15 22:48:15 +0530

    Hero Section

---
+++ src/components/Home/Hero-Section/HeroSection.jsx
@@ -0,0 +1,35 @@
+import "./HeroSection.css"
+
+import React from 'react'
+import bulb from "../../../assets/bulb.png"
+import maskGrp from "../../../assets/Mask_group.png"
+
+const HeroSection = () => {
+    return (
+        <div className="hero-section-wrap">
+            <div className="hero-section-left">
+                <div className="hero-section-left-content">
+                    <div className="hero-left-content-heading">
+                        <h1>DESIGN DRIVEN</h1>
+                        <h1>
+                            <img className="hero-section-left-bulb-image" src={bulb} alt="" />
+                            CREATIVE STRATEGY
+                        </h1>
+                        <h1>FOR GROWTH</h1>
+                    </div>
+
+                    <hr />
+
+                    <p>Transforming Digital Potential into Powerful Reality.</p>
+                </div>
+            </div>
+
+
+            <div className="hero-section-right">
+                <img className="hero-section-right-image" src={maskGrp} alt="" />
+            </div>
+        </div>
+    )
+}
+
+export default HeroSection
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import HomeCards from './Home-Cards/HomeCards.jsx'
 import cardImage from "../../assets/website-development.png"
+import HeroSection from './Hero-Section/HeroSection.jsx'
 
 function Home() {
   return (
     <div className="home-wrapper">
+      <HeroSection/>
       <HomeCards bgColor="#2C0E4A" image={cardImage} heading="Website Design and Development" para="Jincora crafts stunning, functional websites tailored to your business, combining cutting-edge design with seamless functionality to elevate your online presence." />
     </div>
   )
