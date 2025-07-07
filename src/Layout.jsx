a6378e280fa68da8c8b84027a89717a2efd7bd3b
Vaibhav Gupta <112803799+VaibhavGupta2111@users.noreply.github.com>
2025-07-07 17:48:52 +0000

    Background-vid

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,23 +1,34 @@
-import React from 'react'
-import { Outlet } from 'react-router'
-
-import "./App.css"
-import Navbar from './components/NavBar/Navbar.jsx'
-import Footer from './components/Footer/Footer.jsx'
-import Scrollup from './reusableComponents/Scrollup.jsx'
-// import Contact from './components/Contact/Contact.jsx'
+import React from 'react';
+import { Outlet } from 'react-router';
+import './App.css';
+import Navbar from './components/NavBar/Navbar.jsx';
+import Footer from './components/Footer/Footer.jsx';
+import Scrollfea from '../src/reusableComponents/Scrollup.jsx';
 
 function Layout() {
+  return (
+    <div className="main">
+      {/* Video Background */}
+      <video
+        className="background-video"
+        autoPlay
+        loop
+        muted
+        playsInline
+      >
+        <source src="\18458403-hd_1920_1080_24fps.mp4" type="video/mp4" />
+        
+      </video>
 
-    return (
-        <div className="main">
-            <Navbar />
-            <Outlet />
-            {/* <Contact/> */}
-            <Scrollup />
-            <Footer />
-        </div>
-    )
+      {/* Layout content */}
+      <div className="content">
+        <Navbar />
+        <Outlet />
+        <Scrollfea />
+        <Footer />
+      </div>
+    </div>
+  );
 }
 
-export default Layout
+export default Layout;
